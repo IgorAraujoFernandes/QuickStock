071eff97c86d870567d13b87335ebbe48ec8650e
IgorFernandes <igorfernandestec@gmail.com>
2025-05-29 21:44:02 -0300

    First Commit

---
+++ Produto.cs
@@ -0,0 +1,10 @@
+namespace main;
+
+public class Produto
+{
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public int Preco { get; set; }
+    
+}
